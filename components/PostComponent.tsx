5696e642eaf026b92218fb770be7993239438d90
Marco Serrano <marcoTheWebDev@gmail.com>
2019-10-02 11:59:50 -0500

    Added header, with profile picture, username, and date, to PostComponent.

--- components/PostComponent.tsx
+++ components/PostComponent.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Icon from 'react-native-vector-icons/Entypo'
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { Text, View, Image, TouchableOpacity } from 'react-native'
 import { Card, Button } from 'react-native-elements'
 
 export default class Post extends Component {
@@ -21,11 +21,19 @@ export default class Post extends Component {
     }
 
     render() {
+        const header = <>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text style={{ marginLeft: 10, color: '#616161', fontSize: 13 }}>u/jimbob</Text>
+                <Text style={{ marginRight: 10, color: '#616161', fontSize: 12 }}>10/2/19</Text>
+            </View>
+            <Image source={require('../images/bananas.jpg')} style={{ marginLeft: 12, marginTop: 5, width: 55, height: 55 }} />
+            <Text style={{ alignSelf: "center", fontWeight: "bold", fontSize: 18 }}>{'Check this out!'}</Text>
+        </>
         return (
             <Card
-                title='Check this out!'
+                title={header}
                 image={require('../images/bananas.jpg')}>
-                <Text style={{ marginBottom: 10 }}>
+                <Text style={{ alignSelf: 'center', marginBottom: 10 }}>
                     Look at these mf'ing bananas.
                 </Text>
                 <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
